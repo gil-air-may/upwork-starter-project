201d8e8a920568a7f0054c13219f5bcd7d4c4563
Guilherme Gervasio <gilairmay@gmail.com>
2020-02-25 19:00:10 -0300

    pass example iconProps

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { GlobalStyle } from './global-style'
 import { Spinner } from './components/shared/spinner'
+import { randomClassName } from './lib/rcn'
 
 console.info(`⚛️ ${React.version}`)
+//const classNameArray = ['some', 'class', 'name', 'array']
+//console.log(randomClassName()(...classNameArray))
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Spinner />
-  </>
-)
+const iconProps: IconProps = {
+  color: 'some color',
+  className: 'some class name'
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const App = (props: IconProps) => {
+  console.log(props)
+  return (
+    <>
+      <p>Hello Inaccord</p>
+    </>
+  )
+}
+
+ReactDOM.render(<App {...iconProps} />, document.getElementById('root'))
 
 module.hot && module.hot.accept()
